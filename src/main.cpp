9dbcf73d681984d800f9f439f373b2bbe092f956
ShawnHalliday <114366487+ShawnHalliday@users.noreply.github.com>
2023-10-03 14:16:48 -0500

    Imported ARMS

--- src/main.cpp
+++ src/main.cpp
@@ -1,122 +1,21 @@
 #include "main.h"
 #include <string>
+#include "ARMS/api.h"
+#include "ARMS/config.h"
 
-// Left Side Motors
-pros::Motor left_motor_1(1, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor left_motor_2(2, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor_Group left_motors({left_motor_1, left_motor_2});
-
-// // Right Side Motors
-// pros::Motor right_motor_1(3, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
-// pros::Motor right_motor_2(4, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
-// pros::Motor_Group right_motors({right_motor_1, right_motor_2});
-
-// // Drive Train Motors
-// pros::Motor_Group drive_train({left_motor_1, left_motor_2, right_motor_1, right_motor_2});
-
-// // intake motors
-// pros::Motor intake_motor_1(5, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
-// pros::Motor intake_motor_2(6, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
-// pros::Motor_Group intake_motors({intake_motor_1, intake_motor_2});
-
-// // catapult motors
-// pros::Motor catapult_motor(7, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
-
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-		pros::delay(1);
-		pros::Motor motor1(1, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
-		motor1.move(100);
-		pros::delay(20000);	
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
-
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
 void initialize() {
+	arms::init();
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Inititalizing v1");
-	pros::lcd::register_btn1_cb(on_center_button);
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
 void disabled() {}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
 void autonomous() {}
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol() {
-	pros::lcd::set_text(1, "Enters the OPControl");
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	while (true) {
-		left_motor_1.move((master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y)));
-		pros::delay(2);
-		pros::lcd::set_text(1, std::to_string(master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y)));
-
-		left_motor_2.move((master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y)));
-		pros::delay(2);
-		pros::lcd::set_text(1, std::to_string(master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y)));
-
-		// intake_motors.move(127);
-
-		// drive_train.move((master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y)));
-		// pros::delay(2);
-		// pros::lcd::set_text(1, std::to_string(master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y)));
-
-		// right_motors.move((master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X)));
-		// left_motors.move((-(master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X))));
-		// pros::delay(2);
-		// pros::lcd::set_text(1, std::to_string(master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y)));
-
-		// catapult_motor.move((master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)));
-		// pros::delay(2);
-		// pros::lcd::set_text(1, std::to_string(master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)));
+		
     }	
 }
