d631d00945138d363bc97d68de1921792fcf3995
ShawnHalliday <114366487+ShawnHalliday@users.noreply.github.com>
2023-10-12 15:15:38 -0500

    PID Auton Turning Testing

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,10 @@ pros::Motor LM1 (1, pros::E_MOTOR_GEAR_GREEN, false);
 pros::Motor LM2 (2, pros::E_MOTOR_GEAR_GREEN, false);
 pros::Motor RM1 (3, pros::E_MOTOR_GEAR_GREEN, true);
 pros::Motor RM2 (4, pros::E_MOTOR_GEAR_GREEN, true);
-double previousError = 0;
-double intergal = 0;
+double previousErrorL = 0;
+double intergalL = 0;
+double previousErrorR = 0;
+double intergalR = 0;
 pros::Motor_Group LMG({LM1, LM2});
 pros::Motor_Group RMG({RM1, RM2});
 void initialize() {
@@ -46,16 +48,40 @@ double pid(double error, double* pe, double* in, double kp, double ki,
 	return speed;
 }
 
+double pidTurn(double angle) {
+	/*
+	Convert angle to encoder units
+	Runs PID function
+
+	BLUE  (6:1)  : 
+	GREEN (18:1) : 900 Encoder Units / 360 Degrees (900/rev)
+	RED   (36:1) :
+	*/
+	return 0;
+}
 
 void opcontrol() {
+	int i = 0;
+	double targetAngleL = 900;
 	while (true) {
-		double targetD = -10000;
-		double currentD = LMG.get_positions()[1];
-		double error = targetD-currentD;
-		double output = pid(error, &previousError, &intergal, 0.25, 0.002, 0.1);
-		pros::lcd::set_text(0, std::to_string(output) + " " + std::to_string(currentD));
-		LMG.move_velocity(output);
-		// pros::lcd::set_text(0, std::to_string(conRX) + " " + std::to_string(conLY) + " ");// + std::to_string(v) +  std::to_string(v2));
+		// Call PID for each movement motor so it doesn't over/undershoot due to averaging the positions
+		double currentAngleL = LM1.get_position();
+		if(currentAngleL == targetAngleL && i < 250) {
+			targetAngleL += 900;
+			i++;
+		}
+		// double currentAngleL = (LMG.get_positions()[1] + LMG.get_positions()[0])/2;
+		double targetAngleR = -10000;
+		double currentAngleR = (RMG.get_positions()[1] + RMG.get_positions()[0])/2;
+		double errorL = targetAngleL-currentAngleL;
+		double errorR = targetAngleR-currentAngleR;
+		double outputL = pid(errorL, &previousErrorL, &intergalL, 0.25, 0.003, 0.11);
+		double outputR = pid(errorR, &previousErrorR, &intergalR, 0.25, 0.002, 0.1);
+		pros::lcd::set_text(0, "Left: " + std::to_string(outputL) + " " + std::to_string(LM1.get_position()));
+		pros::lcd::set_text(1, std::to_string(i));
+		// pros::lcd::set_text(1, "Right: " + std::to_string(outputR) + " " + std::to_string(currentAngleR));
+		LM1.move_velocity(outputL);
+		//RMG.move_velocity(outputR);
 		pros::delay(10);
     }	
 }
