3f7ae8fd43db1150f4452f90237ca99fd9c6b568
Chess-Dude <68159447+Chess-Dude@users.noreply.github.com>
2023-10-20 12:31:55 -0400

    Inertial Sensor PID turn logic Implemented

--- src/main.cpp
+++ src/main.cpp
@@ -21,11 +21,16 @@ pros::Motor right_motor_3(6, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCOD
 pros::Motor_Group right_motors({right_motor_1, right_motor_2, right_motor_3});
 pros::Motor_Group all_motors({left_motor_1, left_motor_2, left_motor_3, right_motor_1, right_motor_2, right_motor_3});
 
+double previousErrorL[] = {0, 0, 0};
+double intergalL[] = {0, 0, 0};
+double previousErrorR[] = {0, 0, 0};
+double intergalR[] = {0, 0, 0};
+
 // catapult motors
 pros::Motor catapult_motor(3, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
 
 // Assuming the inertial sensor is connected to port 10
-pros::Imu inertial_sensor(10);
+pros::Imu IMU[] = {9, 10};
 
 double pid(double error, double* pe, double* in, double kp, double ki, double kd) {
     double derivative = error - *pe;
@@ -46,6 +51,9 @@ double pid(double error, double* pe, double* in, double kp, double ki, double kd
 void initialize() {
 	arms::init();
 	pros::lcd::initialize();
+    for (int i=0;i < 2; i++) {
+        IMU[i].reset();
+    }
 }
 
 void disabled() {}
@@ -111,151 +119,44 @@ void drive_robot() {
 
 
 void opcontrol() {
-    pros::Task catapult_toggle_task(fire_catapult_toggle);
+    // pros::Task catapult_toggle_task(fire_catapult_toggle);
     pros::Task drive_task(drive_robot);
+    
+    double targetAngle = 90;
 
     while (true) {
-        // Check R2 button to toggle firing
-        if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
-            catapult_shooting = !catapult_shooting;
-            while (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
-                pros::delay(10);
-            }
+
+        for (int i=0;i < 2; i++) {
+            pros::lcd::set_text(i+1, std::to_string(IMU[i].get_yaw()));
         }
 
-        pros::lcd::set_text(3, std::to_string(inertial_sensor.get_yaw()));
+        // Check R2 button to toggle firing
+        // if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+        //     catapult_shooting = !catapult_shooting;
+        //     while (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+        //         pros::delay(10);
+        //     }
+        // }
+
+		double outputL[3];
+		double outputR[3];
+
+        double error_inertial_1 = targetAngle - IMU[0].get_yaw();
+        double error_inertial_2 = targetAngle - IMU[1].get_yaw();
+
+        pros::lcd::set_text(3, std::to_string(error_inertial_1));
+        pros::lcd::set_text(4, std::to_string(error_inertial_2));
+
+        for(int i = 0; i < 3; i++) {
+			outputL[i] = pid(error_inertial_1, &previousErrorL[i], &intergalL[i], 50.0, 0.0005, 0.4);
+			outputR[i] = pid(error_inertial_2, &previousErrorR[i], &intergalR[i], 50.0, 0.0005, 0.4);
+			left_motors[i].move_velocity(outputL[i]);
+			right_motors[i].move_velocity(-outputR[i]);
+            pros::lcd::set_text(5, std::to_string(outputL[i]));
+            pros::lcd::set_text(6, std::to_string(outputR[i]));            
+		}
+		
         pros::delay(10);
 
-        
-
-        // if (master.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
-        //         // Set the desired turning angle in degrees
-        //         inertial_sensor.reset();
-        //         const double target_angle = 90.0;
-
-        //         // Set the motor power for turning
-        //         const int motor_power = 50; // Adjust this value as needed
-
-        //         // Calculate the starting angle
-        //         double start_angle = inertial_sensor.get_yaw();
-
-        //         // Calculate the angle to turn by
-        //         double angle_to_turn = target_angle - start_angle;
-
-        //         // Determine the direction of the turn (left or right)
-        //         int direction = (angle_to_turn > 0) ? 1 : -1;
-
-        //         // While the absolute error is greater than a threshold, keep turning
-        //         while (fabs(angle_to_turn) > 1.0) {
-        //             // Read the current angle from the inertial sensor
-        //             double current_angle = inertial_sensor.get_yaw();
-        //             pros::lcd::set_text(1, ("Current Angle:" + std::to_string(current_angle)));
-
-        //             // Calculate the remaining angle to turn
-        //             angle_to_turn = target_angle - current_angle;
-        //             pros::lcd::set_text(2, ("Remaining Angle to Turn:" + std::to_string(angle_to_turn)));
-
-        //             // Adjust motor power based on the remaining angle
-        //             int power = direction * motor_power;
-
-        //             // Apply power to the left and right drive motors
-        //             // Modify this according to your robot's motor configuration
-
-        //             left_motors.move(power);
-        //             right_motors.move(-power);
-
-        //             // You may also add a delay to control the turn speed
-        //             pros::delay(10); // Adjust the delay as needed
-        //         }
-
-        //         // Stop the motors when the turn is complete
-        //         all_motors.move(0);
-        // }
-
-        // if (master.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
-        //         // Set the desired turning angle in degrees
-        //         const double target_angle = -90.0;
-
-        //         // Set the motor power for turning
-        //         const int motor_power = 50; // Adjust this value as needed
-
-        //         // Calculate the starting angle
-        //         double start_angle = inertial_sensor.get_rotation();
-
-        //         // Calculate the angle to turn by
-        //         double angle_to_turn = target_angle - start_angle;
-
-        //         // Determine the direction of the turn (left or right)
-        //         int direction = (angle_to_turn > 0) ? 1 : -1;
-
-        //         // While the absolute error is greater than a threshold, keep turning
-        //         while (fabs(angle_to_turn) > 1.0) {
-        //             // Read the current angle from the inertial sensor
-        //             double current_angle = inertial_sensor.get_rotation();
-        //             pros::lcd::set_text(1, ("Current Angle:" + std::to_string(current_angle)));
-
-        //             // Calculate the remaining angle to turn
-        //             angle_to_turn = target_angle - current_angle;
-        //             pros::lcd::set_text(2, ("Remaining Angle to Turn:" + std::to_string(angle_to_turn)));
-
-        //             // Adjust motor power based on the remaining angle
-        //             int power = direction * motor_power;
-
-        //             // Apply power to the left and right drive motors
-        //             // Modify this according to your robot's motor configuration
-
-        //             left_motors.move(power);
-        //             right_motors.move(-power);
-
-        //             // You may also add a delay to control the turn speed
-        //             pros::delay(10); // Adjust the delay as needed
-        //         }
-
-        //         // Stop the motors when the turn is complete
-        //         all_motors.move(0);
-        // }
-
-        // if (master.get_digital(pros::E_CONTROLLER_DIGITAL_X)) {
-        //         // Set the desired turning angle in degrees
-        //         const double target_angle = 45.0;
-
-        //         // Set the motor power for turning
-        //         const int motor_power = 50; // Adjust this value as needed
-
-        //         // Calculate the starting angle
-        //         double start_angle = inertial_sensor.get_rotation();
-
-        //         // Calculate the angle to turn by
-        //         double angle_to_turn = target_angle - start_angle;
-
-        //         // Determine the direction of the turn (left or right)
-        //         int direction = (angle_to_turn > 0) ? 1 : -1;
-
-        //         // While the absolute error is greater than a threshold, keep turning
-        //         while (fabs(angle_to_turn) > 1.0) {
-        //             // Read the current angle from the inertial sensor
-        //             double current_angle = inertial_sensor.get_rotation();
-        //             pros::lcd::set_text(1, ("Current Angle:" + std::to_string(current_angle)));
-
-        //             // Calculate the remaining angle to turn
-        //             angle_to_turn = target_angle - current_angle;
-        //             pros::lcd::set_text(2, ("Remaining Angle to Turn:" + std::to_string(angle_to_turn)));
-
-        //             // Adjust motor power based on the remaining angle
-        //             int power = direction * motor_power;
-
-        //             // Apply power to the left and right drive motors
-        //             // Modify this according to your robot's motor configuration
-
-        //             left_motors.move(power);
-        //             right_motors.move(-power);
-
-        //             // You may also add a delay to control the turn speed
-        //             pros::delay(10); // Adjust the delay as needed
-        //         }
-
-        //         // Stop the motors when the turn is complete
-        //         all_motors.move(0);
-        // }
     }
 }
