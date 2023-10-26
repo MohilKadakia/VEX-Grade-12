6dc8d0191dc78e2e843468b83e1fdb4060eb86fc
lbcmk <30442978+lbcmk@users.noreply.github.com>
2023-10-26 12:04:19 -0400

    Split code into seperate files

---
+++ src/header/motors.h
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "ports.h"
+
+// Movement Motors
+// Left Motors
+pros::Motor left_motor_1(PORT1, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor left_motor_2(PORT2, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor left_motor_3(PORT3, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
+
+// Right Motors
+pros::Motor right_motor_1(PORT4, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor right_motor_2(PORT5, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor right_motor_3(PORT6, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
+
+// Motor Groups
+pros::Motor_Group left_motors({left_motor_1, left_motor_2, left_motor_3});
+pros::Motor_Group right_motors({right_motor_1, right_motor_2, right_motor_3});
+
+// Catapult Motor
+pros::Motor catapult_motor(PORT7, pros::E_MOTOR_GEAR_RED, false, pros::E_MOTOR_ENCODER_DEGREES);
+
+// Intake Motor
+pros::Motor intake_motor(PORT8, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
---
+++ src/header/pid.hh
@@ -0,0 +1,4 @@
+#ifndef _pid_HH_
+#define _pid_HH_
+double pid(double error, double* pe, double* in, double kp, double ki, double kd);
+#endif
