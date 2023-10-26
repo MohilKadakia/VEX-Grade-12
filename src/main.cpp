5aa26962d81ed5d0a9680a26b043aa1c2ec4e970
ShawnHalliday <114366487+ShawnHalliday@users.noreply.github.com>
2023-10-26 15:05:59 -0500

    uhhhhhhhhhhhhhhhhhhhhh

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,14 @@
 #include "header/ports.h"
 #include <string>
 
-void initialize() {
-	pros::lcd::initialize();
+bool cata_active = false;
+
+void ResetInertialSensors()
+{
 	for (int i = 0; i < 2; i++){
 		IMU[i].reset();
 	}
 }
-
-void disabled() {}
-
-void competition_initialize() {}
-
-void autonomous() {}
 
 void Drive(pros::Controller master)
 {
@@ -24,6 +20,36 @@ void Drive(pros::Controller master)
 	left_motors.move(std::clamp(moveL, -127, 127));
 	right_motors.move(std::clamp(moveR, -127, 127));
 }
+
+void CatapultTrigger(pros::Controller master, bool* active)
+{	
+	bool cata_button = master.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
+	if (cata_button)
+	{
+		*active = !*active;
+		
+	}
+	pros::delay(20);
+}
+
+void initialize()
+{
+	pros::lcd::initialize();
+	ResetInertialSensors();
+}
+
+void disabled() {}
+
+void competition_initialize()
+{
+	ResetInertialSensors();
+}
+
+void autonomous()
+{
+	ResetInertialSensors();
+}
+
 void DebugValues(pros::Controller master)
 {
 	pros::lcd::set_text(0, "Displacement: LMtop:" + std::to_string(left_motor_1.get_encoder_units()) + " LMfro" + std::to_string(left_motor_2.get_encoder_units()) + " LMbac" + std::to_string(left_motor_3.get_encoder_units()));
@@ -31,14 +57,17 @@ void DebugValues(pros::Controller master)
 	pros::lcd::set_text(2, "RPM: LMtop:" + std::to_string(left_motor_1.get_actual_velocity()) + " LMfro" + std::to_string(left_motor_2.get_actual_velocity()) + " LMbac" + std::to_string(left_motor_3.get_actual_velocity()));
 	pros::lcd::set_text(3, "RPM: RMtop:" + std::to_string(right_motor_1.get_actual_velocity()) + " RMfro" + std::to_string(right_motor_2.get_actual_velocity()) + " RMbac" + std::to_string(right_motor_3.get_actual_velocity()));
 	pros::lcd::set_text(4, "Yaw: ISfro" + std::to_string(IMU[0].get_yaw()) + "ISbac" + std::to_string(IMU[1].get_yaw()));
-
-
+	pros::lcd::set_text(5, "Catapult: " + std::to_string(cata_active));
 }
-void opcontrol() {
+void opcontrol()
+{
+	ResetInertialSensors();
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	while (true) {
+	while (true)
+	{
 		Drive(master);
-		DebugValues(master);
+		DebugValues(master); 
+		//pros::Task myTask(CatapultTrigger, (pros::Controller)"master");
 		pros::delay(10);
     }	
 }
