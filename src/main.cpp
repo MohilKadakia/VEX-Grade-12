89121abb293834c603cc058167282a637d25a03b
ShawnHalliday <114366487+ShawnHalliday@users.noreply.github.com>
2023-10-26 14:02:52 -0500

    Updated All

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,8 @@
 #include "main.h"
-#include "math.h"
+#include "header/devices.h"
+#include "header/pid.hh"
+#include "header/ports.h"
 #include <string>
-#include <iostream>
-#define endl '\n'
-#define PORT1 1
-#define PORT2 2
-#define PORT3 3
-#define PORT4 4
-#define PORT5 5
-#define PORT6 6
-#define PORT7 7
-#define PORT8 8
-#define PORT9 9
-#define PORT10 10
-#define PORT11 11
-#define PORT12 12
-#define PORT13 13
-#define PORT14 14
-#define PORT15 15
-#define PORT16 16
-#define PORT17 17
-#define PORT18 18
-#define PORT19 19
-#define PORT20 20
-
-
-// Left Side Motors
-pros::Motor left_motor_1(PORT1, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor left_motor_2(PORT2, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor left_motor_3(PORT3, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor_Group left_motors({left_motor_1, left_motor_2, left_motor_3});
-// Right Side Motors
-pros::Motor right_motor_1(PORT1, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor right_motor_2(PORT2, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor right_motor_3(PORT3, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor_Group right_motors({right_motor_1, right_motor_2, right_motor_3});
-// Pnemuatic Wings
-pros::ADIDigitalOut wings('h');
-// Inertial Sensors
-pros::IMU IMU[] = {PORT9, PORT10};
-
 
 void initialize() {
 	pros::lcd::initialize();
@@ -52,9 +15,8 @@ void disabled() {}
 
 void competition_initialize() {}
 
-void autonomous() {
-	
-}
+void autonomous() {}
+
 void Drive(pros::Controller master)
 {
 	int moveL = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) + master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
