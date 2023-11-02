6e9da67c9abd97a766cc55647860fddc8d5135f5
Shawn Halliday <114366487+ShawnHalliday@users.noreply.github.com>
2023-11-02 13:59:44 -0400

    Update functions.hh

--- src/header/functions.hh
+++ src/header/functions.hh
@@ -1,8 +1,12 @@
 #ifndef _functions_HH_
 #define _functions_HH_
 bool catapult_active;
+double pid_previous_time;
+double pid_previous_error;
+double pid_intergal;
 double debug_values(bool* active);
 double catapult_trigger(bool* active);
 void debug_values();
 void drive();
+double pid(double error, double* pe, double* in, double kp, double ki, double kd);
 #endif
