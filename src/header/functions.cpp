3e127f6ab7489769dc418a2197fb82e93cf0f76a
Shawn Halliday <114366487+ShawnHalliday@users.noreply.github.com>
2023-11-02 14:02:49 -0400

    Update functions.cpp

--- src/header/functions.cpp
+++ src/header/functions.cpp
@@ -21,6 +21,21 @@ double pid(double error, double* pe, double* in, double kp, double ki, double kd
 
     return speed;
 }
+double pidReal(double error, double* pe, double* pt, double* in, double kp, double ki, double kd)
+{
+    double current_time = (double) pros::micros()/1000000.0;
+    double derivative = (error - *pe)/(current_time - *pt);
+    //If it overshoots reset integral
+    if ((*pe > 0 && error < 0) || (*pe < 0 && error > 0))
+    {
+        *in = 0;
+    }
+    *in += (*pe * (current_time - *pt)) + ((error - *pe)*(current_time - *pt)*(0.5));
+    double speed = error * kp + *in * ki + derivative * kd;
+    *pe = error;
+    *pt = current_time;
+    return speed;
+}
 void debug_values()
 {
     while(true)
