ef60ea788fb3155df01ab9ce6935dc35aae06930
lbcmk <30442978+lbcmk@users.noreply.github.com>
2023-11-02 13:54:54 -0400

    Renamed functions to catapult

--- src/header/catapult.hh
+++ src/header/catapult.hh
@@ -1,5 +1,5 @@
-#ifndef _functions_HH_
-#define _functions_HH_
+#ifndef _catapult_HH_
+#define _catapult_HH_
 bool catapult_active;
 double debug_values(bool* active);
 double catapult_trigger(bool* active);
