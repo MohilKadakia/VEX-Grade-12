f7cb17916dca0f06cfac8b3a594d73ceee47b98e
Shawn Halliday <114366487+ShawnHalliday@users.noreply.github.com>
2023-11-02 14:00:22 -0400

    Update functions.cpp

--- src/header/functions.cpp
+++ src/header/functions.cpp
@@ -2,7 +2,25 @@
 #include "devices.h"
 
 bool catapult_active = false;
+double pid_previous_time = 0;
+double pid_previous_error = 0;
+double pid_intergal = 0;
 
+double pid(double error, double* pe, double* in, double kp, double ki, double kd) {
+    double derivative = error - *pe;
+    if ((*pe > 0 && error < 0) || (*pe < 0 && error > 0))
+      *in = 0; // remove integral at zero error
+    double speed = error * kp + *in * ki + derivative * kd;
+
+    // only let integral wind up if near the target
+    if (fabs(error) < 15) {
+    *in += error;
+    }
+
+    *pe = error;
+
+    return speed;
+}
 void debug_values()
 {
     while(true)
