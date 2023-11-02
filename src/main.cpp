8415ae93186a5297b99755c92fd2f99aa62c3047
ShawnHalliday <114366487+ShawnHalliday@users.noreply.github.com>
2023-11-02 10:40:28 -0500

    fart smella

--- src/main.cpp
+++ src/main.cpp
@@ -49,13 +49,13 @@ void autonomous()
 	}
 }
 void debug_values()
-void catapult_trigger(bool* active)
+void catapult_trigger()
 {	
-	// bool cata_button = master.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
 	while(true) {
 		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
 		{
-			&active = !&active;
+			catapult_active = !catapult_active;
+			pros::delay(90);
 		}
 		pros::delay(10);
 	}
@@ -66,14 +66,14 @@ void catapult_trigger(bool* active)
 	pros::lcd::set_text(2, "RPM: LMtop:" + std::to_string(left_motor_1.get_actual_velocity()) + " LMfro" + std::to_string(left_motor_2.get_actual_velocity()) + " LMbac" + std::to_string(left_motor_3.get_actual_velocity()));
 	pros::lcd::set_text(3, "RPM: RMtop:" + std::to_string(right_motor_1.get_actual_velocity()) + " RMfro" + std::to_string(right_motor_2.get_actual_velocity()) + " RMbac" + std::to_string(right_motor_3.get_actual_velocity()));
 	pros::lcd::set_text(4, "Yaw: ISfro" + std::to_string(IMU[0].get_yaw()) + "ISbac" + std::to_string(IMU[1].get_yaw()));
-	pros::lcd::set_text(5, "Catapult: " + std::to_string(cata_active));
+	pros::lcd::set_text(5, "Catapult: " + std::to_string(catapult_active));
 }
 void opcontrol()
 {
 	pros::lcd::clear();
 	ResetInertialSensors();
 
-	pros::Task cata_trigger(catapult_trigger (bool*)catapult_active);
+	pros::Task cata_trigger(catapult_trigger);
 	while (true)
 	{
 		drive();
