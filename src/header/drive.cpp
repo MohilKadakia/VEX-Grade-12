02bea6f0e6a85d03e98b92f867a0fa0fdd329dbf
ShawnHalliday <114366487+ShawnHalliday@users.noreply.github.com>
2023-11-30 15:17:44 -0500

    Drive to allow for reverse controls

--- src/header/drive.cpp
+++ src/header/drive.cpp
@@ -6,20 +6,19 @@ double move_multiple(double x) {
 }
 
 void drive_robot() {
+	double multiple = 1.0;
 	while (true) {
-		double multiple = 1.0;
-
 		if(master.get_digital(master_UP)) {
-			multiple = (double)(std::clamp((int)(multiple*100) +  25, 25, 100))/100;
+			multiple = 1;
 		} else if (master.get_digital(master_DOWN)) {
-			multiple = (double)(std::clamp((int)(multiple*100) - 25, 25, 100))/100;
+			multiple = -1;
 		}
 
-		int left_y = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+		int left_y = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) * multiple;
 		int right_x = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X) * -1;
 		
-        int left_motor_velocity = (left_y + (right_x * move_multiple(right_x)) * multiple) * 1;
-		int right_motor_velocity = (left_y - (right_x * move_multiple(right_x)) * multiple) * 1;
+        int left_motor_velocity = (left_y + (right_x * move_multiple(right_x)));
+		int right_motor_velocity = (left_y - (right_x * move_multiple(right_x))) ;
 
 		left_motors.move(std::clamp(left_motor_velocity, -127, 127));
 		right_motors.move(std::clamp(right_motor_velocity, -127, 127));
