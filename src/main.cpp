f0740803b77a668cd71aa3e8a1291e972e7640bc
Chess-Dude <68159447+Chess-Dude@users.noreply.github.com>
2023-12-12 14:25:35 -0500

    added fail safe for catapult shooting

--- src/main.cpp
+++ src/main.cpp
@@ -44,12 +44,24 @@ void autonomous() {
 	move_backward(50.0, -90.0, 90.0);
 	left_motors.move(25);
 	// turn_right_to_look_at(80);
-	pros::delay(300);
+	pros::delay(600);
 	left_motors.move(0);
 
 	catapult_motors.move(87);
-	pros::delay(30000);
-	catapult_motors.move(0);
+	pros::delay(500);
+	uint32_t current_time = pros::millis();
+	while(pros::millis() < current_time + 30000) {
+		catapult_motors.move(87);
+		if(abs(catapult_motor_1.get_actual_velocity()) < 1) {
+			catapult_motors.move(0);
+			pros::delay(400);
+			catapult_motors.move(87);
+			pros::delay(1000);
+		}
+		pros::delay(10);
+	}
+	// pros::delay(30000);
+	// catapult_motors.move(0);
 	// pros::delay(5000);
 	move_forward_inertial(20.0, -50.0, 50.0, 40.0);
 	reset_inertial();
