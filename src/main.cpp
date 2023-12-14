6d22bb7a68e40630bcd75ede33b5f80c57ce271b
ShawnHalliday <114366487+ShawnHalliday@users.noreply.github.com>
2023-12-14 11:31:00 -0500

    maybe works

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,15 @@
 // 	reset_inertial();
 // }
 
+void hold(double holdValue, double holdTime){
+	double startTime = pros::millis();
+	double upperLimit = startTime+(holdTime*1000);
+	while (pros::millis() < upperLimit){
+		pros::lcd::set_text(0, std::to_string(std::clamp((upperLimit-pros::millis())/1000, holdValue, 127.0)*(holdValue*holdTime)));
+		master.set_text(0 , 0, std::to_string(std::clamp((upperLimit-pros::millis())/1000, holdValue, 127.0)*(holdValue*holdTime)));
+	}
+}
+
 void disabled() 
 {
 	pros::lcd::clear();
@@ -35,19 +44,25 @@ void competition_initialize() {
 void autonomous() {
 
 
-	move_forward_inertial(500, -80.0, 80.0, 60.0);
-
-	// pros::delay(5000);
-
-	// move_forward_inertial(200.0, -80.0, 80.0, 50.0);
-
-	// // wings.set_value(1);
-	// left_motors.move(-110);
-	// right_motors.move(-80);
-	// // wings.set_value(0);
-	// pros::delay(1400);
+	hold(32, 6);
+	// //Defense Side
+	// left_motors.move(-83);
+	// right_motors.move(-127);
+	// pros::delay(1000);
 	// left_motors.move(0);
 	// right_motors.move(0);
+	// pros::delay(500);
+	// left_motors.move(65);
+	// right_motors.move(127);
+	// pros::delay(100);
+	// wings.set_value(1);
+	// pros::delay(700);
+	// wings.set_value(0);
+	// left_motors.move(0);
+	// right_motors.move(0);
+	// pros::delay(100);
+
+	// move_forward_inertial(147, 25, 75, 50);
 
 	// pros::delay(100);
 	// left_motors.move(110);
