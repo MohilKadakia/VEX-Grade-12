a00a6c1f804b61a42cee526b0d261fbd19653d26
ShawnHalliday <114366487+ShawnHalliday@users.noreply.github.com>
2024-02-13 14:51:54 -0500

    cleaned up more code, todo: move radius function

--- src/header/auto.cpp
+++ src/header/auto.cpp
@@ -1,11 +1,25 @@
 #include "devices.hh"
 #include "functions.hh"
 #include "auto.hh"
+using namespace std;
+double pid_distance_previous_error;
+double pid_distance_integral;
+double pid_turn_previous_error;
+double pid_turn_integral;
 
-void move(double meters) {
-    double travelled;
-    while (travelled < meters){
-
+void move(double meters, double encode_unit_to_meter, double kP, double kI, double kD, double kPturn, double kIturn, double kDturn) {
+    double travelled = 0;
+    double start_angle = IMU.get_yaw();
+    double turned = 0;
+    IMU.tare_yaw();
+    while (abs(travelled) < abs(meters)){
+        travelled = encode_unit_to_meter*(left_motors.get_positions+right_motors.get_positions)/2
+        pid_distance_output = pid(meters-travelled, &pid_distance_previous_error, &pid_distance_integral, kP, kI, kD)
+        turned += IMU.get_yaw()-turned;
+        IMU.tare_yaw();
+        pid_turn_output = pid(defrees-turned, &pid_turn_previous_error, &pid_turn_integral, kPturn, kIturn, kDturn);
+        left_motors.set_value(pid_distance_output+pid_turn_output);
+        right_motors.set_value(pid_distance_output-pid_turn_output);
     }
 }
 
@@ -13,14 +27,16 @@ void move_radius() {
 
 }
 
-void turn(double degrees) {
+void turn(double degrees, double kP, double kI, double kD) {
     double start_angle = IMU.get_yaw();
     double turned = 0;
     IMU.tare_yaw();
-    while (turned < degrees) {
+    while (abs(turned) < abs(degrees)) {
         turned += IMU.get_yaw()-turned;
         IMU.tare_yaw();
+        pid_turn_output = pid(defrees-turned, &pid_turn_previous_error, &pid_turn_integral, kP, kI, kD);
+        left_motors.set_value(pid_turn_output);
+        right_motors.set_value(-pid_turn_output);
     }
 }
-using namespace std;
 
--- src/header/auto.hh
+++ src/header/auto.hh
@@ -1,4 +1,6 @@
 #ifndef _auto_HH_
 #define _auto_HH_
-
+void move();
+void move_radius();
+void turn();
 #endif
