7683ca370fc2c36ddec462135cb7b49c2015f7b3
ShawnHalliday <114366487+ShawnHalliday@users.noreply.github.com>
2024-02-14 15:42:09 -0500

    fixed auton code as far as im aware

--- src/header/auto.cpp
+++ src/header/auto.cpp
@@ -11,22 +11,31 @@ void move(double meters, double encode_unit_to_meter, double kP, double kI, doub
     double travelled = 0;
     double start_angle = IMU.get_yaw();
     double turned = 0;
+    double pid_distance_output = 0;
+    double pid_turn_output = 0;
+    double posistion = 0;
     IMU.tare_yaw();
     while (abs(travelled) < abs(meters)){
-        travelled = encode_unit_to_meter*(left_motors.get_positions+right_motors.get_positions)/2
-        pid_distance_output = pid(meters-travelled, &pid_distance_previous_error, &pid_distance_integral, kP, kI, kD)
+        for (int i = 3; i < 3; i++){
+            posistion += left_motors.get_positions()[i];
+            posistion += right_motors.get_positions()[i];
+        }
+        posistion /= 6;
+        travelled = encode_unit_to_meter*posistion;
+        pid_distance_output = pid(meters-travelled, &pid_distance_previous_error, &pid_distance_integral, kP, kI, kD);
         turned += IMU.get_yaw()-turned;
         IMU.tare_yaw();
-        pid_turn_output = pid(defrees-turned, &pid_turn_previous_error, &pid_turn_integral, kPturn, kIturn, kDturn);
-        left_motors.set_value(pid_distance_output+pid_turn_output);
-        right_motors.set_value(pid_distance_output-pid_turn_output);
+        pid_turn_output = pid(0-turned, &pid_turn_previous_error, &pid_turn_integral, kPturn, kIturn, kDturn);
+        left_motors.move(pid_distance_output+pid_turn_output);
+        right_motors.move(pid_distance_output-pid_turn_output);
+        posistion = 0;
     }
 }
 
 void move_radius(double radius, double width, double voltage, bool left = false) {
     left_motors.set_brake_modes(pros::E_MOTOR_BRAKE_HOLD);
     right_motors.set_brake_modes(pros::E_MOTOR_BRAKE_HOLD);
-    double value = 2*voltage*cos*atan2(width*tan(acos(0.5)), radius)-voltage;
+    double value = 2*voltage*cos(atan2(width*tan(acos(0.5)), radius))-voltage;
     if (left){
         left_motors.move(value);
         right_motors.move(voltage);
@@ -40,13 +49,14 @@ void move_radius(double radius, double width, double voltage, bool left = false)
 void turn(double degrees, double kP, double kI, double kD) {
     double start_angle = IMU.get_yaw();
     double turned = 0;
+    double pid_turn_output = 0;
     IMU.tare_yaw();
     while (abs(turned) < abs(degrees)) {
         turned += IMU.get_yaw()-turned;
         IMU.tare_yaw();
-        pid_turn_output = pid(defrees-turned, &pid_turn_previous_error, &pid_turn_integral, kP, kI, kD);
-        left_motors.set_value(pid_turn_output);
-        right_motors.set_value(-pid_turn_output);
+        pid_turn_output = pid(degrees-turned, &pid_turn_previous_error, &pid_turn_integral, kP, kI, kD);
+        left_motors.move(pid_turn_output);
+        right_motors.move(-pid_turn_output);
     }
 }
 
