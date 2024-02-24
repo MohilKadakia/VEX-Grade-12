91d4d4e9bc1b94c5a003c39097f47f48215fdb17
ShawnHalliday <114366487+ShawnHalliday@users.noreply.github.com>
2024-02-23 22:49:48 -0500

    commented auton

--- src/main.cpp
+++ src/main.cpp
@@ -27,99 +27,99 @@ void competition_initialize() {
 }
 
 void autonomous() {
-	// move_forward_inertial_pid(15, -127, 100);
 	pros::delay(50);
+
 	left_motors.move(92);
 	right_motors.move(100);
-	pros::delay(100);
+	pros::delay(100);  //Space robot from wall
 	left_motors.move(0);
 	right_motors.move(0);
-	pros::delay(100);
+	pros::delay(50);
+
 	turn(-90, 1.2, 0.2, 1.6);
 	left_motors.move(88);
 	right_motors.move(94);
 	intake_motor.move(127);
-	pros::delay(760); // At Post
+	pros::delay(760); // Turn and drive to post and intake
 	left_motors.move(0);
 	right_motors.move(0);
 	pros::delay(1100);
+
 	intake_motor.move(10);
 	left_motors.move(-92);
 	right_motors.move(-100);
-	pros::delay(750); //Drive Back
+	pros::delay(750);   //Drive back to origin
 	left_motors.move(0);
 	right_motors.move(0);
-	turn(135, 1.2, 0.2, 1.6); //Turn to net
+
+
+	turn(135, 1.2, 0.2, 1.6); 
 	intake_motor.move(-127);
 	left_motors.move(88);
 	right_motors.move(94);
-	pros::delay(400); // Push towards net
+	pros::delay(400); // Turn to net and outtake and push triballs towards net
 	left_motors.move(0);
 	right_motors.move(0);
-	// left_motors.move(92);
-	// right_motors.move(127);
-	// pros::delay(750);
-	// left_motors.move(-20);
-	// right_motors.move(-25);
 	pros::delay(200);
-	turn(-110, 1.2, 0.2, 1.6); // Turn to mid
+
+	turn(-110, 1.2, 0.2, 1.6); 
 	left_motors.move(88);
 	right_motors.move(94);
-	pros::delay(500);
+	pros::delay(500); // Turn and start driving towards middle triball
 	left_motors.move(0);
 	right_motors.move(0);
-	turn(25, 1.2, 0.2, 1.6); // Thread the needle
+
+	turn(25, 1.2, 0.2, 1.6);
 	left_motors.move(70);
 	right_motors.move(78);
 	intake_motor.move(127);
-	pros::delay(1300); //Pick up triball
+	pros::delay(1300); // Turn again to fit between triballs and intake far middle ball
 	left_motors.move(0);
 	right_motors.move(0);
 	pros::delay(100);
-	turn(120, 1.2, 0.2, 1.6); // Turn to net
+
+	turn(120, 1.2, 0.2, 1.6);
 	pros::delay(200);
 	intake_motor.move(-127);
-	left_motors.move(100);
+	left_motors.move(100); // Turn to net and push in 2 triballs
 	right_motors.move(110);
-	pros::delay(900); // Push into net
+	pros::delay(900);
+
 	left_motors.move(-88);
 	right_motors.move(-98);
-	pros::delay(300); // Drive away
+	pros::delay(300);   // space away from net
 	left_motors.move(0);
 	right_motors.move(0);
-	turn(150, 1.2, 0.2, 1.6); // turn to corner ball
+
+	turn(150, 1.2, 0.2, 1.6);
 	intake_motor.move(127);
 	left_motors.move(80);
 	right_motors.move(90);
-	pros::delay(850);// Drive to cornet ball
+	pros::delay(850); //Turn, drive to, and intake corner ball
 	left_motors.move(0);
 	right_motors.move(0);
 	pros::delay(100);
-	turn(-110, 1.2, 0.2, 1.6); // turn to og corner
+
+	turn(-110, 1.2, 0.2, 1.6); 
 	left_motors.move(80);
 	right_motors.move(90);
-	pros::delay(1400); // Drive there
+	pros::delay(1400); // Turn and drive to origin corner
 	left_motors.move(0);
 	right_motors.move(0);
-	turn(-150, 1.2, 0.2, 1.6); // turn to og corner
+
+	turn(-150, 1.2, 0.2, 1.6); 
 	intake_motor.move(-127);
-	left_motors.move(80);
+	left_motors.move(80); // Turn and drive to push in final triballs
 	right_motors.move(90);
-	pros::delay(1000); // Dr
+	pros::delay(1000);
+
 	left_motors.move(-50);
 	right_motors.move(-50);
 	pros::delay(200);
-	left_motors.move(110);
+	left_motors.move(110); // Do Second Hit
 	right_motors.move(127);
 	pros::delay(700);
 	intake_motor.move(0);
-
-	
-
-
-
-	// move_forward_inertial_pid(100, -1, 100);
-
 }
 
 void opcontrol() {
